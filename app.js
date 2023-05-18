182f966daf2c19eafa15b27df1f1c4e49c457145
theopmw <theowrightdev@gmail.com>
2023-05-18 07:54:12 +0100

    Add logic to reset input and comments

--- app.js
+++ app.js
@@ -14,13 +14,16 @@ const app = Vue.createApp({
       alert('Submitted');
     },
     setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
+      this.name = event.target.value;
     },
     add(num) {
       this.counter = this.counter + num;
     },
     reduce(num) {
       this.counter = this.counter - num;
+    },
+    resetInput() {
+      this.name = '';
     },
   },
 });
