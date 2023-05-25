2a02f4781cc0959eed6412e1252e26dfb00845fc
theopmw <theowrightdev@gmail.com>
2023-05-25 15:33:28 +0100

    30. Working with Watchers

--- app.js
+++ app.js
@@ -3,17 +3,43 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: '',
+      // lastName: '',
       confirmedName: '',
+      fullname: '',
     };
   },
   computed: {
     fullname() {
       console.log('Running...');
-      if (this.name === '') {
+      if (this.name === '' || this.lastName === '') {
         return '';
       }
-      return this.name + ' ' + 'Wright';
+      return this.name + ' ' + this.lastName;
     },
+  },
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        const that = this;
+        setTimeout(function () {
+          that.counter = 0;
+        }, 2000);
+      }
+    },
+    // name(value) {
+    //   if (value === '') {
+    //     this.fullname === '';
+    //   } else {
+    //     this.fullname = value + ' ' + this.lastName;
+    //   }
+    // },
+    // lastName(value) {
+    //   if (value === '') {
+    //     this.fullname === '';
+    //   } else {
+    //     this.fullname = this.name + ' ' + value;
+    //   }
+    // },
   },
   methods: {
     outputFullName() {
